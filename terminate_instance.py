e0fb9ee8d02e3170bfdfb4320f81d0bce2164d2f
Sunil Surendran <sunil.surendran.cse@gmail.com>
2018-06-15 13:23:25 +0530

    Edited code. Correction in variable

--- terminate_instance.py
+++ terminate_instance.py
@@ -6,6 +6,6 @@ import sys
 ec2 =  boto3.resource('ec2')
 for instance_id in sys.argv[1:]:
 	instance = ec2.Instance(instance_id)
-	response = intance.terminate()
+	response = instance.terminate()
 	print response
 
