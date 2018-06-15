fe6c11821d5350b49b1250c3bb42561dc3aae354
Sunil Surendran <sunil.surendran.cse@gmail.com>
2018-06-15 13:12:07 +0530

    Added a python scipt to create instances

---
+++ create_instance.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+import boto3
+
+ec2 = boto3.resource('ec2')
+instance = ec2.create_instances(
+	ImageId = 'ami-db710fa3',
+	MinCount = 1,
+	MaxCount = 1,
+	InstanceType = 't2.micro')
+print instance[0].id
